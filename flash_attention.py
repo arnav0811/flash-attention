d5f8b95e6fad7df875eb470bcff58402d0385e71
Arnav Mardia <64305658+arnav0811@users.noreply.github.com>
2025-06-24 11:30:26 -0700

    Triton Kernel

---
+++ flash_attention.py
@@ -0,0 +1,64 @@
+import torch
+import triton
+import triton.language as tl
+
+@triton.jit
+def flash_attention_kernel(
+  Q, K, V, O, 
+  seq_len, head_dim,
+  BLOCK_SIZE_M: tl.constexpr, # Query Block Size
+  BLOCK_SIZE_N: tl.constexpr, #K/V Block Size
+):
+  # Q, K, V, O are pointers
+  # Index of program
+  program_id_m = tl.program_id(0) #1-D launch grid, axis = 0
+  # Program will process inputs that are offset from the initital data. 
+  #[O:Block Size, Block Size : 2xBlock Size,...]
+  start_m = program_id_m * BLOCK_SIZE_M
+
+  # Offsets for Query Matrix
+  # rows
+  offsets_m = start_m + tl.arange(0, BLOCK_SIZE_M)
+  # columns
+  offsets_k = tl.arange(0, head_dim)
+
+  # [Block_size_m, head_dim]
+  Q_block = tl.load(Q + offsets_m[:, None] * head_dim + offsets_k[None, :])
+
+  # O = PV
+  accumulator = tl.zeros([BLOCK_SIZE_M, head_dim], dtype = tl.float32)
+  
+  # log sum exp trick
+  # maximum current raw attention score for each query
+  max_i = tl.full([BLOCK_SIZE_M], float('-inf'), dtype=tl.float32) 
+  # sum of exp 
+  exp_sum_i = tl.zeros([BLOCK_SIZE_M], dtype=tl.float32)
+
+  for start_n in range(0, seq_len, BLOCK_SIZE_N):
+    offsets_n = start_n + tl.arange(0, BLOCK_SIZE_N)
+    K_block = tl.load(K + offsets_n[:, None] * head_dim + offsets_k[None, :])
+    V_block = tl.load(V + offsets_n[:, None] * head_dim + offsets_k[None, :])
+
+    # S = QK^T
+    S = tl.dot(Q_block, tl.trans(K_block))
+
+    # Prevent underflow/overflow Softmax
+    max_ij = tl.max(S, axis = 1)
+    # f(x)'s in softmax eq
+    S_new = S - max_ij[:, None]
+
+    exp_S = tl.exp(S_new)
+    l_ij = tl.sum(exp_S, axis = 1)
+
+    accumulator *= (exp_sum_i / (exp_sum_i + l_ij))[:, None]
+    accumulator += tl.dot(exp_S, V_block)
+
+    # Update for softmax
+    max_i = tl.maximum(max_i, max_ij)
+    exp_sum_i += l_ij
+
+  # Output Block woith Memory addr calc
+  tl.store(0 + offsets_m[:, None] * head_dim + offsets_k[None, :], accumulator)
+    
+  
+  
