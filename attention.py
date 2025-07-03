5ffc94d81e33d92cc8ca12e9df4477884a6c9ae1
Arnav Mardia <64305658+arnav0811@users.noreply.github.com>
2025-07-03 14:36:51 -0700

    Scaling factor

--- attention.py
+++ attention.py
@@ -4,7 +4,7 @@ import math
 
 def scaled_dot_product_attention(Q, K, V, mask = None):
   d_k = Q.size(-1)
-  scores = torch.matmul(Q, K.transpose(-2, -1))
+  scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
   if mask is not None:
     scores = scores.masked_fill(mask == 0, float('-inf'))
   attention_weights = F.softmax(scores, dim = -1)
